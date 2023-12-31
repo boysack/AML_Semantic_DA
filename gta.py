5194f9aaeb936574f762d151ec57ed6d944bb376
Alessio Cappello s309450 <alessio.cappello@studenti.polito.it>
2023-12-31 13:05:36 +0100

    gta dataset class

--- gta.py
+++ gta.py
@@ -1,25 +1,35 @@
 from torch.utils.data import Dataset
 from pathlib import Path
 from PIL import Image
-from torchvision.transforms import Compose, Resize, ToTensor
+from torchvision.transforms import Compose, Resize, ToTensor, Grayscale
 import random
 # TODO
 
 
 class GTA(Dataset):
-    def __init__(self):
+    def __init__(self, mode):
         super(GTA, self).__init__()
-        self.root_samples = Path(r"./Datasets/GTA5/images")
-        self.root_labels = Path(r"./Datasets/GTA5/labels")
-        self.transform = Compose([Resize((512, 1024), interpolation=Image.NEAREST), ToTensor()])
+        if mode == "train":
+            root_samples = Path(r"./Datasets/GTA5/images/train")
+            root_labels = Path(r"./Datasets/GTA5/labels/train")
+        elif mode == "val":
+            root_samples = Path(r"./Datasets/GTA5/images/val")
+            root_labels = Path(r"./Datasets/GTA5/labels/val")
+        else:
+            raise Exception()
+        
+        self.root_samples = root_samples
+        self.root_labels = root_labels
+        self.transform1 = Compose([Resize((512, 1024), interpolation=Image.NEAREST), ToTensor()])
+        self.transform2 = Compose([Resize((512, 1024), interpolation=Image.NEAREST), Grayscale(), ToTensor()])
         self.samples = self._collect_samples()
         
 
     def __getitem__(self, idx):
         path, label = self.samples[idx]
         img1 = Image.open(path)
-        img2 = Image.open(label)  ## < qui sono 3 canali, gestire
-        return self.transform(img1), self.transform(img2)
+        img2 = Image.open(label) 
+        return self.transform1(img1), 255*self.transform2(img2)
 
 
     def __len__(self):
@@ -31,14 +41,16 @@ class GTA(Dataset):
         
         Helper method for the constructor
         """
-        sample_path += self._collect_imgs_sub_dir(self.root_samples)
-        label_path += self._collect_imgs_sub_dir(self.root_labels)
+        samples = []
+        labels = []
 
-        sample_path = sorted(sample_path)
-        label_path = sorted(label_path)
+        samples += self._collect_imgs_sub_dir(self.root_samples)
+        labels += self._collect_imgs_sub_dir(self.root_labels)
 
-        samples = list(zip(sample_path, label_path))
-        random.shuffle(samples)
+        samples = sorted(samples)
+        labels = sorted(labels)
+
+        return list(zip(samples, labels))
 
 
     @staticmethod
