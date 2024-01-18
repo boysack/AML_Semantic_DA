06fb35ebad69b77342f1115f25d7e0ec5caaa2b9
Alessio Cappello s309450 <alessio.cappello@studenti.polito.it>
2024-01-18 22:24:52 +0100

    Revert "Fourier Hashmap"
    This reverts commit e8ea40bd2d10b9a0ebfa5d6f1a4c11581448a91e.

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import time
 import torch.nn as nn
 import torch
 from torch.nn import functional as F
@@ -300,7 +299,6 @@ def group_weight(weight_group, module, norm_layer, lr):
 def low_freq_mutate_np( amp_src, amp_trg, L=0.1 ):
     a_src = np.fft.fftshift( amp_src, axes=(-2, -1) )
     a_trg = np.fft.fftshift( amp_trg, axes=(-2, -1) )
-    
 
     _, h, w = a_src.shape
     b = (  np.floor(np.amin((h,w))*L)  ).astype(int)
@@ -311,43 +309,35 @@ def low_freq_mutate_np( amp_src, amp_trg, L=0.1 ):
     h2 = c_h+b+1
     w1 = c_w-b
     w2 = c_w+b+1
-    
+
     a_src[:,h1:h2,w1:w2] = a_trg[:,h1:h2,w1:w2]
     a_src = np.fft.ifftshift( a_src, axes=(-2, -1) )
     return a_src
 
-Fourier = {}
-def FDA_source_to_target_np( src_img_np, trg_img_np, L=0.1 ):
+
+def FDA_source_to_target_np( src_img, trg_img, L=0.1 ):
     # exchange magnitude
     # input: src_img, trg_img
-    # get fft of both source and target
-    
-    key_src = str(src_img_np)
-    if key_src not in Fourier:
-        fft_src_np = np.fft.fft2( src_img_np, axes=(-2, -1) )
-        amp_src, pha_src = np.abs(fft_src_np), np.angle(fft_src_np)
-        pha_src_exp= np.exp( 1j * pha_src )
-        Fourier[key_src] = (amp_src, pha_src_exp )
-    else:
-        amp_src, pha_src_exp = Fourier[key_src]
 
-    key_trg = str(trg_img_np)
-    if key_trg not in Fourier:		
-        fft_trg_np = np.fft.fft2( trg_img_np, axes=(-2, -1) )
-        amp_trg, _= np.abs(fft_trg_np), np.angle(fft_trg_np)
-        Fourier[key_trg] = amp_trg
-    else:
-        amp_trg = Fourier[key_trg]
+    src_img_np = src_img #.cpu().numpy()
+    trg_img_np = trg_img #.cpu().numpy()
+
+    # get fft of both source and target
+    fft_src_np = np.fft.fft2( src_img_np, axes=(-2, -1) )
+    fft_trg_np = np.fft.fft2( trg_img_np, axes=(-2, -1) )
+
+    # extract amplitude and phase of both ffts
+    amp_src, pha_src = np.abs(fft_src_np), np.angle(fft_src_np)
+    amp_trg, pha_trg = np.abs(fft_trg_np), np.angle(fft_trg_np)
 
     # mutate the amplitude part of source with target
     amp_src_ = low_freq_mutate_np( amp_src, amp_trg, L=L )
 
     # mutated fft of source
-    fft_src_ = amp_src_ * pha_src_exp
+    fft_src_ = amp_src_ * np.exp( 1j * pha_src )
 
     # get the mutated image
     src_in_trg = np.fft.ifft2( fft_src_, axes=(-2, -1) )
     src_in_trg = np.real(src_in_trg)
-
 
     return src_in_trg
