7eb372933d52e8bee420a504c5f4ed51a7ec1a63
crazyForos <s317616@studenti.polito.it>
2024-01-21 16:00:39 +0100

    float16

--- CreateSSL.py
+++ CreateSSL.py
@@ -66,8 +66,8 @@ def main():
 
     # change the mean for different dataset
 
-    predicted_label = np.zeros((len(targetloader), 1024,2048))
-    predicted_prob = np.zeros((len(targetloader), 1024,2048))
+    predicted_label = np.zeros((len(targetloader), 1024,2048), dtype=np.float16)
+    predicted_prob = np.zeros((len(targetloader), 1024,2048), dtype=np.float16)
     image_name = []
 
     with torch.no_grad():
