2bf45e6e86c54d12fd6d500aa5ddcb84c817db9f
crazyForos <s317616@studenti.polito.it>
2024-01-21 15:08:33 +0100

    eval

--- evaluation_multi.py
+++ evaluation_multi.py
@@ -1,11 +1,27 @@
-from PIL import Image
 import torch.nn as nn
 import torch
 import numpy as np
-import os
-import scipy.io as sio
+from utils import reverse_one_hot, compute_global_accuracy, fast_hist, per_class_iu
+import argparse
+from cityscapes import CityScapes
+from torch.utils.data import DataLoader
+
+def parse_args():
+    parse = argparse.ArgumentParser()
+
+    parse.add_argument('--num_classes',
+                       type=int,
+                       default=19,
+                       help='num of object classes (with void)')
+    parse.add_argument('--num_workers',
+                       type=int,
+                       default=2,
+                       help='num of workers')
+
+    return parse.parse_args()
 
 def main():
+    args = parse_args()
     """ model1 = CreateModel(args)
     model1.eval()
     model1.cuda()
@@ -14,33 +30,51 @@ def main():
     model2.eval()
     model2.cuda() """
 
-
-    targetloader = CreateTrgDataLoader(args)
+    val_dataset = CityScapes(mode='val')
+    targetloader = DataLoader(val_dataset,
+                       batch_size=1,
+                       shuffle=False,
+                       num_workers=args.num_workers,
+                       drop_last=False)
 
     # ------------------------------------------------- #
     # compute scores and save them
     with torch.no_grad():
-        for image, _, name in targetloader:
+        precision_record = []
+        hist = np.zeros((args.num_classes, args.num_classes))
+        for image, label, _ in targetloader:
             # forward
-            output1 = model1(image)
+            output1, _, _ = model1(image)
             output1 = nn.functional.softmax(output1, dim=1)
 
-            output2 = model2(image)
+            output2, _, _ = model2(image)
             output2 = nn.functional.softmax(output2, dim=1)
 
             a, b = 0.5, 0.5
-            output = a*output1 + b*output2 
+            predict = a*output1 + b*output2 
+            predict = reverse_one_hot(predict)
+            predict = np.array(predict.cpu())
 
-            output = output.transpose(1,2,0)
+            # get RGB label image
+            label = label.squeeze()
+            label = np.array(label.cpu())
 
-            output_nomask = np.asarray( np.argmax(output, axis=2), dtype=np.uint8 )
-            output_nomask = Image.fromarray(output_nomask)    
-            name = name[0].split('/')[-1]
-            output_nomask.save(  '%s/%s' % (args.save, name)  )
+            # compute per pixel accuracy
+            precision = compute_global_accuracy(predict, label)
+            hist += fast_hist(label.flatten(), predict.flatten(), args.num_classes)
 
-    # scores computed and saved
-    # ------------------------------------------------- #
-    compute_mIoU( args.gt_dir, args.save, args.devkit_dir, args.restore_from )    
+            # there is no need to transform the one-hot array to visual RGB array
+            # predict = colour_code_segmentation(np.array(predict), label_info)
+            # label = colour_code_segmentation(np.array(label), label_info)
+            precision_record.append(precision)
+
+        precision = np.mean(precision_record)
+        miou_list = per_class_iu(hist)
+        miou = np.mean(miou_list)
+        print('precision per pixel for test: %.3f' % precision)
+        print('mIoU for validation: %.3f' % miou)
+        print(f'mIoU per class: {miou_list}')
+
 
 
 if __name__ == '__main__':
